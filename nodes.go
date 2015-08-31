5c4ecace4988c86a6897f23b0ad7ca98027866f3
Xavier Shay <xavier@squareup.com>
2015-08-31 10:41:07 -0700

    Prepare nodeLocalClusterLookup for taking an expression rather than a literal.

--- nodes.go
+++ nodes.go
@@ -36,7 +36,7 @@ type nodeRegexp struct {
 }
 
 type nodeLocalClusterLookup struct {
-	key string
+	node parserNode
 }
 
 type nodeGroupQuery struct {
@@ -101,7 +101,7 @@ func (n nodeGroupQuery) String() string {
 }
 
 func (n nodeLocalClusterLookup) String() string {
-	return fmt.Sprintf("$%s", n.key)
+	return fmt.Sprintf("$%s", n.node)
 }
 
 func (n nodeBraces) String() string {
